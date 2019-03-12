fadd153fa11b8b9bce6827ff9af2792bbb4ab84f
cetecom <cetecom@LCV10823>
2019-03-11 22:33:41 -0300

    estilos

---
+++ ejemplo03/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width" />
+        <link href="estilos.css" rel="stylesheet" type="text/css"/>
+        <title>Estilos</title>
+    </head>
+    <body>
+        <?php
+        ?>
+            <div id="menu">MENU</div>
+            <div id="head">HEAD</div>
+            <div id="contenedor">CONTENEDOR</div>
+    </body>
+</html>
